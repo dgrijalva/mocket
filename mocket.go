80e089a279aa412415b7cc31aee27a4901c5e3c4
Dave Grijalva <grijalva@gmail.com>
2011-10-13 19:42:31 -0700

    basics

--- mocket.go
+++ mocket.go
@@ -1,1 +1,67 @@
 package mocket
+
+import "net"
+import "bytes"
+import "io"
+import "os"
+
+type Mocket struct {
+	server, client *bytes.Buffer
+	closed bool
+}
+
+func New()*Mocket {
+	m := &Mocket{server: &bytes.Buffer{}, client: &bytes.Buffer{}}
+	return m
+}
+
+func (m *Mocket) Server()net.Conn {
+	return &side{m, m.server, m.client}
+}
+
+func (m *Mocket) Client()net.Conn {
+	return &side{m, m.client, m.server}
+}
+
+func (m *Mocket) Close() {
+	
+}
+
+type side struct {
+	m *Mocket
+	r io.Reader
+	w io.Writer
+}
+
+func (s *side) Read(p []byte) (n int, err os.Error) {
+	return s.r.Read(p)
+}
+
+func (s *side) Write(p []byte) (n int, err os.Error) {
+	return s.w.Write(p)
+}
+
+func (s *side) LocalAddr() net.Addr {
+	return nil
+}
+
+func (s *side) RemoteAddr() net.Addr {
+	return nil
+}
+
+func (s *side) SetTimeout(nsec int64) os.Error {
+	return nil
+}
+
+func (s *side) SetReadTimeout(nsec int64) os.Error {
+	return nil
+}
+
+func (s *side) SetWriteTimeout(nsec int64) os.Error {
+	return nil
+}
+
+func (s *side) Close() os.Error {
+	s.m.Close()
+	return nil
+}
--- mocket_test.go
+++ mocket_test.go
@@ -1,1 +1,37 @@
 package mocket
+
+import (
+	"testing"
+	"net"
+	"bytes"
+)
+
+func TestSetup(t *testing.T) {
+	m := New()
+	if _, ok := m.Server().(net.Conn); !ok {
+		t.Errorf("mocket.Server() should be a net.Conn")
+	}
+	if _, ok := m.Client().(net.Conn); !ok {
+		t.Errorf("mocket.Client() should be a net.Conn")
+	}
+}
+
+func TestBasicRW(t *testing.T) {
+	m := New()
+	c, s := m.Client(), m.Server()
+	
+	data := []byte("ping")
+	
+	c.Write(data)
+	r := make([]byte, 4)
+	l, e := s.Read(r)
+	if i := 4; l != i {
+		t.Errorf("length of server read should be %v was %v", i, l)
+	}
+	if e != nil {
+		t.Errorf("server read should not produce error, was %v", e)
+	}
+	if !bytes.Equal(data, r) {
+		t.Errorf("expected %v was %v", data, r)
+	}
+}
